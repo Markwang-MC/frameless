72537413dae8d5afe98701c45c856b95fdd67920
jimmy Wang <Jimmy.w.xuanyu@gmail.com>
2024-08-28 08:47:13 -0400

    update

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const {bodyParser,serveStatic} = require('./middleware.js')
 const esbuildPluginFrameworkless = require('./plugin.js')
 
+
+
 module.exports = {bodyParser,serveStatic,esbuildPluginFrameworkless}
